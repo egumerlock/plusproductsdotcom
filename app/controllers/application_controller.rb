ae13e697c3066be5bf58f00e5502cdee34d41d05
Eric Gumerlock <eric.gumerlock@gmail.com>
2016-10-20 11:34:49 -0700

    Working app and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def home
+
+  end
+
+  def products
+
+  end
+
+  def store_finder
+  end
+
+  def team
+
+  end
+
 end
